d76bca52baba4629370d459187b223057d6b7b8b
sh0shinsha <merletim@gmail.com>
2019-06-24 21:34:38 -0400

    high score?

--- test/levellord.js
+++ test/levellord.js
@@ -1,9 +1,23 @@
 import {expect} from 'chai';
 import OrmBase from '../src/index.js';
 
+class Mode extends OrmBase {
+  static idTablePath() {
+    return "test/json/level/mode-whitelist.json"
+  }
+
+  levels() {
+    return this.getChildren(Level);
+  }
+}
+
 class Level extends OrmBase {
   static idTablePath() {
     return "test/json/level/level-whitelist.json"
+  }
+
+  mode() {
+    return this.getParent(Mode);
   }
 
   lessons() {
@@ -21,28 +35,58 @@ class Lesson extends OrmBase {
   }
 }
 
+describe("Parent class Mode", () => {
+  it("should have correct attributes from json file", () => {
+    let a = new Mode("adventure");
+    expect(a.funFactor).to.equal("5 Bags of Popcorn");
+  });
+
+  it("should have children of type Level", ()=> {
+    let a = new Mode("adventure");
+    let levels = a.levels();
+    expect(levels.length).to.be.equal(1);
+    levels.forEach(child => expect(child).to.be.an.instanceof(Level));
+  });
+})
+
 describe("Parent class Level", () => {
   it("should have correct attributes from json file", () => {
-    let a = new Level("biomes");
-    expect(a.name).to.equal("Biomes");
+    let b = new Level("biomes");
+    expect(b.name).to.equal("Biomes");
   });
 
   it("should have children of type Lesson", ()=> {
-    let a = new Level("biomes");
-    let lessons = a.lessons();
+    let b = new Level("biomes");
+    let lessons = b.lessons();
     expect(lessons.length).to.be.equal(1);
     lessons.forEach(child => expect(child).to.be.an.instanceof(Lesson));
   });
+
+  it("should have a parent of type Mode", ()=> {
+    let b = new Level("biomes");
+    expect(b.mode()).to.be.an.instanceof(Mode);
+  });
+
+  it("should have the correct parent", ()=> {
+    let b = new Level("biomes");
+    expect(b.mode().name).to.equal("Adventure");
+  });
+  
 })
 
 describe("Child class Lesson", () => {
   it("should have correct attributes from json file", () => {
-    let b = new Lesson("fish");
-    expect(b.ages).to.equal("5+");
+    let c = new Lesson("fish");
+    expect(c.ages).to.equal("5+");
   });
 
   it("should have a parent of type Level", ()=> {
-    let b = new Lesson("fish");
-    expect(b.level()).to.be.an.instanceof(Level);
+    let c = new Lesson("fish");
+    expect(c.level()).to.be.an.instanceof(Level);
+  });
+
+  it("should have the correct parent", ()=> {
+    let c = new Lesson("fish");
+    expect(c.level().name).to.equal("Biomes");
   });
 })
