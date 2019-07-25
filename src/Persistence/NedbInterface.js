1c609980d638bc2ce9197818b29a88ef6d82dc71
Elliot Berman <elliotjberman@gmail.com>
2019-07-24 22:18:29 -0400

    ?

---
+++ src/Persistence/NedbInterface.js
@@ -0,0 +1,115 @@
+// TODO: Make this work without electron
+
+import path from 'path';
+import electron from 'electron';
+import Datastore from 'nedb';
+
+const USER_DATA_DIRECTORY = (electron.app || electron.remote.app).getPath('userData');
+const DB_SINGLETONS = {}; // Note: A rarely sanctioned use of globals
+
+
+// Helpers
+function createDoc(modelInstance, isNew) {
+  const doc = {};
+
+  const allSchema = {...modelInstance.schema, ...modelInstance.parentRelationalSchema};
+  Object.keys(allSchema).forEach(key => {
+    const expectedType = allSchema[key];
+    const value = modelInstance[key];
+    if(!value instanceof expectedType)
+      throw Error(`Schema error: key '${key}' is of type ${typeof value}, not ${expectedType}`)
+
+    doc[key] = value;
+  });
+
+  return doc;
+}
+
+function getDbSingleton(collection) {
+  if (DB_SINGLETONS[collection] === undefined)
+    DB_SINGLETONS[collection] = new Datastore({ filename: path.join(USER_DATA_DIRECTORY, collection + ".db"), autoload: true, timestampData: true });
+  return DB_SINGLETONS[collection];
+}
+
+async function insertModel(db, modelInstance) {
+  return new Promise((resolve, reject) => {
+    const doc = createDoc(modelInstance);
+
+    db.insert(doc, (err, newDoc) => {
+      if (err) reject(err);
+      modelInstance.id = newDoc._id;
+      resolve(modelInstance);
+    });
+  });
+}
+
+async function updateModel(db, modelInstance) {
+  return new Promise((resolve, reject) => {
+    const doc = createDoc(modelInstance);
+
+    db.update({_id: modelInstance.id}, {$set: doc}, {}, (err) => {
+      if (err) reject(err);
+      resolve(modelInstance);
+    });
+  });
+}
+
+async function saveModel(modelInstance) {
+  const db = getDbSingleton(modelInstance.constructor.tableName);
+  if (!modelInstance.id) {
+    await insertModel(db, modelInstance);
+    return;
+  }
+  await updateModel(db, modelInstance);
+}
+
+async function deleteModel(modelInstance) {
+  if (!modelInstance.id)
+    throw Error(`Trying to delete instance of ${modelInstance.constructor.name} that has no id. Have you saved it?`);
+
+  return new Promise((resolve, reject) => {
+    const db = getDbSingleton(modelInstance.constructor.tableName);
+    db.remove({_id: modelInstance.id}, {}, (err, docs) => {
+      if (err) reject(err);
+      resolve(docs);
+    });
+  });
+}
+
+async function getModelById(tableName, id) {
+  return new Promise((resolve, reject) => {
+    const db = getDbSingleton(tableName);
+    db.findOne({_id: id}, (err, doc) => {
+      if (err) reject(err);
+      resolve(doc);
+    });
+  });
+}
+
+async function filterRecords(tableName, query) {
+  return new Promise((resolve, reject) => {
+    const db = getDbSingleton(tableName);
+    db.find(query, {}, (err, docs) => {
+      if (err) reject(err);
+      resolve(docs || []);
+    });
+  });
+}
+
+async function countRecords(tableName, query) {
+  return new Promise((resolve, reject) => {
+    const db = getDbSingleton(tableName);
+    db.count(query, (err, count) => {
+      if (err) reject(err);
+      resolve(count);
+    });
+  });
+}
+
+export default {
+  saveModel,
+  deleteModel,
+  getModelById,
+  filterRecords,
+  countRecords
+}
